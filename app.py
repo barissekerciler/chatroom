fcfd2dab53c0b6f07c2655a0c34c4b09d2246a1d
barissekerciler <sekercilerbaris@gmail.com>
2019-02-19 17:33:40 +0300

    endpoint changed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/chatroom', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def chat():
     redi_ = RedisOperations()
     if request.method == 'GET':
