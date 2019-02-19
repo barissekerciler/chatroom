aada7a6175c76398370c5c23021ff66cd0330542
barissekerciler <sekercilerbaris@gmail.com>
2019-02-19 23:36:41 +0300

    updates

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask
+from flask import Flask, request, render_template, Response
 from flask_cors import CORS
-from flask import jsonify
-from flask import request
 from redis_operations import RedisOperations
 import configparser
 
@@ -16,11 +14,16 @@ CORS(app)
 def chat():
     redi_ = RedisOperations()
     if request.method == 'GET':
-        return jsonify(result=redi_.get_messages())
+        return render_template('index.html', data=redi_.get_messages())
 
     if request.method == 'POST':
-        return jsonify(result=redi_.save_message(username=request.json['username'],
-                                                 message=request.json['message']))
+        redi_.save_message(username=request.form['username'], message=request.form['message'])
+        return render_template('index.html', data=redi_.get_messages())
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return Response(status=200)
 
 
 if __name__ == '__main__':
